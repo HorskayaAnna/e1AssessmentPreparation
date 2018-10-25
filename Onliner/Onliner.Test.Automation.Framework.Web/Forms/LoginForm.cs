fa4aab1167acc050f45a3fc39d94937861f9599a
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-10-25 18:22:05 +0300

    add some new class

---
+++ Onliner/Onliner.Test.Automation.Framework.Web/Forms/LoginForm.cs
@@ -0,0 +1,27 @@
+﻿using Onliner.Test.Automation.Framework.Web.Controls;
+using OpenQA.Selenium;
+
+namespace Onliner.Test.Automation.Framework.Web.Forms
+{
+    class LoginForm
+    {
+        public TextBox login = new TextBox(By.CssSelector("input[data - field = login]"));
+        public TextBox password = new TextBox(By.CssSelector("input[type=password]"));
+        public Button submit = new Button(By.XPath("//button[@type='submit']"));
+
+        public void inputData(string loginData, string passwordData)
+        {
+            login.SendKeys(loginData);
+            password.SendKeys(passwordData);
+        }
+
+        public void clickSubmit()
+        {
+            submit.Click();
+        }
+
+        public bool isSubmitClickable() => submit.IsClickable();
+        public bool isLoginTextBoxVisible() => login.IsVisible();
+        public bool isPassTextBoxVisible() => password.IsVisible();
+    }
+}
---
+++ Onliner/Onliner.Test.Automation.Framework.Web/Forms/TextBox.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Onliner.Test.Automation.Framework.Web.Controls
+{
+    class TextBox : BaseElement
+    {
+        public TextBox(By locator) : base(locator)
+        {
+        }
+    }
+}
